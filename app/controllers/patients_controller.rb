30e3a847cebd6a1069a7748b7555d1bc009864c9
Alex J Fowler <alexjfowler@gmail.com>
2017-02-16 06:46:04 +0000

    updated logic

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,3 +1,6 @@
+#4. Show calculation
+# -- Raj Thanguraja (Thursday Afternoons)
+
 class PatientsController < ApplicationController
   helper_method :calculate_dose, :ibw, :ddw, :crcl
   
@@ -18,29 +21,42 @@ end
 
   def crcl()
       	if @patient.gender == "Female"
-      		a = ((140-@patient.age)*(@patient.weight))/@patient.creatinine
+      		a = ((140-@patient.age)*(@ibw)*1.04)/@patient.creatinine
       	else
-      		a = ((140-@patient.age)*(@patient.weight*1.2))/@patient.creatinine
+      		a = ((140-@patient.age)*(@ibw)*1.23)/@patient.creatinine
         end
         
-        if a <=30 
-      		puts "use renal dosing"
-      	else
-      		"Creatinine Clearance is #{a}ml/min, safe to use normal dosing"
-      	end
+        if a <= 20
+          return "#{a.to_i}ml/min, NOT SAFE TO USE NORMAL DOSING -- contact pharmacy"
+        
+        else
+          return "#{a.to_i}ml/min, safe to use normal dosing"
     end
-def ibw
-  a = 154
-	diff = (@patient.height.to_i) - a
-	if @patient.gender == "Male"
-		@ibw =  50 + (diff*(0.91))
-	else 
-		@ibw =  45 + (diff*(0.91))
-	end
+  end
+    
+#def ibw
+ # b = 154
+	#diff = (@patient.height.to_i) - b
+	#if @patient.gender == "Male"
+	#	@ibw =  50 + (diff*(0.91))
+	#else 
+	#	@ibw =  45 + (diff*(0.91))
+	#end
   
-  return @ibw
-end
-
+  #return @ibw
+  #end
+  def ibw
+    inch = (@patient.height*(0.393))
+    diff = inch.to_i - 60
+    
+    if @patient.gender == "Male"
+      @ibw = 50 + (diff * 2.3)
+    else
+      @ibw = 45.5 + (diff * 2.3)
+    end
+    return @ibw
+  end
+  
 def ddw
   @ddw = @ibw+(0.4*(@patient.weight.to_i - @ibw))
 end
@@ -56,7 +72,11 @@ def calculate_dose()
 		@dose = @ibw.round*5
 	end
   
+  if @dose >480
+  return "Calculated dose of #{@dose} is higher than the recommended maximum dose of 480mg"
+else
   return @dose
+end
 end
 
 private
